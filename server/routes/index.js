f4277d3e70a0692488ecdb4aae29c801fc1bcd01
Ethnosqq <ais200101@gmail.com>
2022-11-19 13:39:03 +0600

    Initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,6 @@ const typeRouter = require('./typeRouter')
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
 router.use('/brand', brandRouter)
-router.use('/device', productRouter)
+router.use('/product', productRouter)
 
 module.exports = router
